55d14c45391ebec5bf367d531e62ee57d15cd003
biblin <james@jamestolley.net>
2015-09-18 16:15:39 +0100

    Role service documentation

--- src/services/ServiceError.js
+++ src/services/ServiceError.js
@@ -1,3 +1,8 @@
+/**
+ * Generic error class for services to throw when they encounter bad parameters
+ * Default error handler should convert these to BadRequest responses instead of 
+ * InternalServerError
+ */
 function ServiceError(message) {
 	this.name = "ServiceError";
 	this.message = (message || "");
