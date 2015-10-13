b2b60b138ed696ec7a29f8db03e50914f877ce95
JamesTolley <james@jamestolley.net>
2015-10-13 18:23:23 +0100

    Some roles Controller unit tests

--- test/controllerTests/rolesController.js
+++ test/controllerTests/rolesController.js
@@ -8,7 +8,7 @@ var should = require('should'),
 	controller = rewire('../../src/controllers/roles'),
 	ServiceError = require('../../src/services/ServiceError');
 
-/*global describe, it*/	
+/*global describe, it, beforeEach*/	
 describe('Role Management', function() {
 	
 	var next = function(e) {
@@ -50,7 +50,103 @@ describe('Role Management', function() {
 											
 			})				
 		})
-	})
+	});
+	
+	describe('Role list', function() {
+		
+		beforeEach(function() {
+			var mockRoles = [
+				{
+					id: 1,
+					name: 'role1'
+				},
+				{
+					id: 2,
+					name: 'role2'
+				}
+			];
+			
+			var mockService = {
+				roles: function() { return Promise.resolve(mockRoles); }
+			}
+			
+			controller.__set__('roleService', mockService);
+		});
+		
+		it('Should list roles as embedded resources', function() {
+			
+			var 
+				req = {},
+				res = mockResponse();
+				
+			controller.getRoles(req, res, next);
+				
+			return res.then(function(response) {
+				response.body.should.have.property('_embedded');
+				response.body._embedded.should.have.property('roles');
+				response.body._embedded.roles.length.should.equal(2);
+			});
+		});
+		
+		it('Should include a create link', function() {
+			var 
+				req = {},
+				res = mockResponse();
+				
+			controller.getRoles(req, res, next);
+				
+			return res.then(function(response) {
+				response.body._links.should.have.property('create');
+			});			
+		});
+		
+		it('Should include a link to each role included', function() {
+			var 
+				req = {},
+				res = mockResponse();
+				
+			controller.getRoles(req, res, next);
+				
+			return res.then(function(response) {
+				response.body._embedded.roles[0]._links.should.have.property('self');
+			});			
+		});
+		
+	});
+	
+	describe('Permissions list', function() {
+		
+		it('Should list permissions array as the main resource', function() {
+			var 
+				req = {},
+				res = mockResponse();
+				
+			var mockPermissions = [
+				{
+					id: 1,
+					name: 'permission1'
+				},
+				{
+					id: 2,
+					name: 'permission2'
+				}
+			];
+			
+			var mockService = {
+				permissions: function() { return Promise.resolve(mockPermissions); }
+			}
+			
+			controller.__set__('roleService', mockService);				
+				
+			controller.getPermissions(req, res, next);
+				
+			return res.then(function(response) {
+				response.body.should.have.property('permissions');
+				response.body.permissions.length.should.equal(2);
+			});				
+		})
+		
+	});
 
 	
 });
